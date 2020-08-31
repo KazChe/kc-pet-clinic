00d2c9cd43d1ad74e12ae13a690cc48431ddc05a
KazChe <kaz.che@gmail.com>
2020-08-31 11:58:59 -0700

    initial commit

---
+++ src/main/java/com/petclinic/kcpetclinic/KcPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.petclinic.kcpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KcPetClinicApplication.class, args);
+    }
+
+}
