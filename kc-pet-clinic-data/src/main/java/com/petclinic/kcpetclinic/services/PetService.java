6cb6bda588b94d19dad79197d37596f556ca2647
KazChe <kaz.che@gmail.com>
2020-09-02 17:03:35 -0700

    Created base crud service interface. closes #11

--- kc-pet-clinic-data/src/main/java/com/petclinic/kcpetclinic/services/PetService.java
+++ kc-pet-clinic-data/src/main/java/com/petclinic/kcpetclinic/services/PetService.java
@@ -1,15 +1,8 @@
 package com.petclinic.kcpetclinic.services;
 
-import com.petclinic.kcpetclinic.model.Owner;
 import com.petclinic.kcpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
 
-    Pet findById(Long id);
-
-    Pet save(Owner owner);
-
-    Set<Pet> findAll();
 }
--- kc-pet-clinic-data/src/main/java/com/petclinic/kcpetclinic/services/VetService.java
+++ kc-pet-clinic-data/src/main/java/com/petclinic/kcpetclinic/services/VetService.java
@@ -1,15 +1,7 @@
 package com.petclinic.kcpetclinic.services;
 
-import com.petclinic.kcpetclinic.model.Pet;
 import com.petclinic.kcpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set <Pet> findAll();
 }
