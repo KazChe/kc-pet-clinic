5b93f9bb115bab1c76281ddf5e10d858aab78d07
KazChe <kaz.che@gmail.com>
2020-09-04 18:47:18 -0700

    Load bootstrap data on startup. closes #14

--- kc-pet-clinic-web/src/main/java/com/petclinic/kcpetclinic/bootstrap/DataLoader.java
+++ kc-pet-clinic-web/src/main/java/com/petclinic/kcpetclinic/bootstrap/DataLoader.java
@@ -1,5 +1,7 @@
 package com.petclinic.kcpetclinic.bootstrap;
 
+import com.petclinic.kcpetclinic.model.Owner;
+import com.petclinic.kcpetclinic.model.Vet;
 import com.petclinic.kcpetclinic.services.OwnerService;
 import com.petclinic.kcpetclinic.services.VetService;
 import com.petclinic.kcpetclinic.services.map.OwnerServiceMap;
@@ -21,6 +23,34 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        Owner owner1 = new Owner();
+        owner1.setFirstName("Kimia");
+        owner1.setLastName("Chehresa");
+        owner1.setId(100L);
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setFirstName("Kaz");
+        owner2.setLastName("Chehresa");
+        owner2.setId(101L);
+        ownerService.save(owner2);
+
+        System.out.println("....Owner data created...");
+
+        Vet vet1 = new Vet();
+        vet1.setFirstName("Gooz");
+        vet1.setLastName("Goozian");
+        vet1.setId(600L);
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setFirstName("Chosu");
+        vet2.setLastName("Chosian");
+        vet2.setId(600L);
+        vetService.save(vet2);
+
+        System.out.println("....Vet data created...");
 
     }
 }
