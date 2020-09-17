a1d209e350f1da85efc27f49acb73c7cdc186bac
KazChe <kaz.che@gmail.com>
2020-09-17 01:56:16 -0700

    Create Specialty Map Service Impl. Closes #48

---
+++ kc-pet-clinic-data/src/main/java/com/petclinic/kcpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.petclinic.kcpetclinic.services;
+
+import com.petclinic.kcpetclinic.model.Specialty;
+import com.sun.xml.bind.v2.model.core.ID;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
